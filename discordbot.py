79dddba6e31488bc353a90b09268b06040f90461
javateaboy <noikari73@gmail.com>
2020-02-25 21:34:35 +0900

    no message

--- discordbot.py
+++ discordbot.py
@@ -4,6 +4,7 @@ import os
 TOKEN = os.environ['DISCORD_BOT_TOKEN']
 
 client = discord.Client()
+guild = discord.Guild
 
 # 起動時
 @client.event
@@ -17,7 +18,7 @@ async def on_message(message):
     if message.author.bot:
         return
     # /nekoでにゃーんと帰る
-    if message.content == '/neko':
+    if message.content.startwith("南雲"):
         await message.channel.send('にゃーん')
 
 client.run(TOKEN)
